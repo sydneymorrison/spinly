63faae5f7d5c74b90f2625be5bbcfe53a74e3192
sydneymorrison <smorrison23@gsb.columbia.edu>
2023-05-15 14:52:49 -0400

    Update vinyl model with vinyl schema, Update review schema

--- models/vinyl.js
+++ models/vinyl.js
@@ -1,3 +1,76 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+
+const vinylSchema = new Schema ({
+    title: {
+        type: String,
+        required: true
+    },
+    artist: {
+        type: String,
+        required: true
+    },
+    releaseYear: {
+        type: Number,
+        default: function() {
+          return new Date().getFullYear();
+        }
+      },
+    genre: {
+        type: String,
+        required: true
+    },
+    condition: {
+        type: String,
+        enum: ['Poor', 'Fair', 'Good', 'Very Good', 'Mint']
+    },
+    exchangeType: {
+        type: String,
+        enum: ['Trade', 'Sell', 'Buy']
+    },
+    userId: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    username: String,
+    userAvatar: String,
+    reviews: [reviewSchema]
+}, {
+    timestamps: true
+});
+
+
+const reviewSchema = new Schema ({
+    userId: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    vinylId: {
+        type: Schema.Types.ObjectId,
+        ref: 'Vinyl',
+        required: true
+    },
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5
+    },
+    comment: {
+        type: String,
+        required: true
+    },
+    postedAt: {
+        type: Date,
+        default: Date.now
+    }
+}, {
+    timestamps: true
+});
+
+
+
+// Compile the schema into a model and export it
+module.exports = mongoose.model('Vinyl', vinylSchema);
