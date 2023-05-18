f29b32c470378dc1381881b440bad9b468df7059
sydneymorrison <smorrison23@gsb.columbia.edu>
2023-05-18 01:34:41 -0400

    Update CSS & HTML for Landing Page

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -11,61 +11,6 @@ module.exports = {
 
 
 
-//   async function create(req, res){
-//     const vinylId = req.params.id;
-//     const userId = req.user._id;
-//     try {
-//         //Find the vinyl post by its vinylId and user Id
-//         const vinyl = await Vinyl.findOne({ _id: vinylId, user: userId });
-//         //if the user doesn't exist
-//         if (!vinyl) { 
-//             return res.redirect(`/vinyls/${vinylId}`);
-//     }
-//         //Add the user-centric info to the req.body for the new review
-//         req.body.user = req.user._id;
-//         req.body.userName = req.user.name;
-//         req.body.userAvatar = req.user.avatar;
-
-//         //Push or unshift subdocs into Mongoose arrays
-//         vinyl.reviews.push(req.body);
-
-//         //Save the updated vinyl doc
-//         await vinyl.save();
-
-//     } catch (err) {
-//         console.log(err);
-//         res.redirect(`/vinyls/${vinylId}`);
-//     }
-
-// }
-
-
-//   async function deleteReview(req, res){
-//     const vinylId = req.params.id;
-//     const userId = req.user._id;
-//     try {
-//         //Find the vinyl post by its vinylId and user Id
-//         const vinyl = await Vinyl.findOne({ _id: vinylId, user: userId });
-//         //if the user doesn't exist
-//         if (!vinyl) { 
-//             return res.redirect(`/vinyls/${vinylId}`);
-//     }
-//         //Remove the review using the remove method available on the Moongoose arrays
-//         vinyl.reviews.remove(vinylId);
-
-//         //Save the updated movie doc
-//         await vinyl.save();
-
-//         //Redirect back to the movie's show view
-//         res.redirect(`/vinyls/${vinylId}`);
-
-//     } catch (err) {
-//         console.log(err);
-//         res.redirect(`/vinyls/${vinylId}`);
-//     }
-//   }
-
-
 async function create(req, res) {
     const vinyl = await Vinyl.findById(req.params.id);
   
@@ -87,7 +32,7 @@ async function create(req, res) {
 
 
 async function deleteReview(req, res) {
-  // Note the cool "dot" syntax to query on the property of a subdoc
+  // Query properties on a subdoc
   const vinyl = await Vinyl.findOne({ 'reviews._id': req.params.id, 'reviews.user': req.user._id });
   // Rogue user!
   if (!vinyl) return res.redirect('/vinyls');
